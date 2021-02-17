0948cb96b4d262cc8929daa77a96f263af2e672b
adighd <69061151+adighd@users.noreply.github.com>
2021-02-16 18:21:28 -0800

    Update footer.jsx

--- js/footer.jsx
+++ js/footer.jsx
@@ -23,39 +23,78 @@ var app = app || {};
 			}
 
 			var nowShowing = this.props.nowShowing;
-			return (
-				<footer className="footer">
-					<span className="todo-count">
-						<strong>{this.props.count}</strong> items left
-					</span>
-					<ul className="filters">
-						<li>
-							<a
-								href="#/"
-								className={classNames({selected: nowShowing === app.ALL_TODOS})}>
-									All
-							</a>
-						</li>
-						{' '}
-						<li>
-							<a
-								href="#/active"
-								className={classNames({selected: nowShowing === app.ACTIVE_TODOS})}>
-									Active
-							</a>
-						</li>
-						{' '}
-						<li>
-							<a
-								href="#/completed"
-								className={classNames({selected: nowShowing === app.COMPLETED_TODOS})}>
-									Completed
-							</a>
-						</li>
-					</ul>
-					{clearButton}
-				</footer>
-			);
+			//Changes items to be plural or single depending on props count
+			if (this.props.count < 2) {
+				return (
+					<footer className="footer">
+						<span className="todo-count">
+							<strong>{this.props.count}</strong> item left
+						</span>
+						<ul className="filters">
+							<li>
+								<a
+									href="#/"
+									className={classNames({selected: nowShowing === app.ALL_TODOS})}>
+										All
+								</a>
+							</li>
+							{' '}
+							<li>
+								<a
+									href="#/active"
+									className={classNames({selected: nowShowing === app.ACTIVE_TODOS})}>
+										Active
+								</a>
+							</li>
+							{' '}
+							<li>
+								<a
+									href="#/completed"
+									className={classNames({selected: nowShowing === app.COMPLETED_TODOS})}>
+										Completed
+								</a>
+							</li>
+						</ul>
+						{clearButton}
+					</footer>
+				);
+			} else {
+				return (
+					<footer className="footer">
+						<span className="todo-count">
+							<strong>{this.props.count}</strong> items left
+						</span>
+						<ul className="filters">
+							<li>
+								<a
+									href="#/"
+									className={classNames({selected: nowShowing === app.ALL_TODOS})}>
+										All
+								</a>
+							</li>
+							{' '}
+							<li>
+								<a
+									href="#/active"
+									className={classNames({selected: nowShowing === app.ACTIVE_TODOS})}>
+										Active
+								</a>
+							</li>
+							{' '}
+							<li>
+								<a
+									href="#/completed"
+									className={classNames({selected: nowShowing === app.COMPLETED_TODOS})}>
+										Completed
+								</a>
+							</li>
+						</ul>
+						{clearButton}
+					</footer>
+				);
+				
+				
+			}	
 		}
 	});
 })();
